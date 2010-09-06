b00f5a37517b906077e26783714a967c0ff36c4c
Peter Kordel <pkordel@gmail.com>
2010-09-06 20:02:06 +0200

    testing with webrat

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
   def home
+    @title = "Ruby on Rails Tutorial Sample App | Home"
   end
 
   def contact
+    @title = "Ruby on Rails Tutorial Sample App | Contact"
+  end
+  
+  def about
+    @title = "Ruby on Rails Tutorial Sample App | About"
   end
 
 end
